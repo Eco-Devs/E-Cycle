2072cfd82cab23259d12d0f38a7cada94a1666c2
LorenBll <lorenzo.bellotta@hotmail.com>
2025-04-03 10:27:10 +0200

    Changed main.

--- src/main/java/ecodevs/ecycle/EcycleApplication.java
+++ src/main/java/ecodevs/ecycle/EcycleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcycleApplication {
 
-	public static void main(String[] args) {
+	public static void main ( String[] args ) {
 		SpringApplication.run(EcycleApplication.class, args);
 	}
 
